aa2c7318fe4c3f5ebadb5b3e15a13804858b21e8
MahmoudAlsonbaty <mahmoudAlsonbaty1@gmail.com>
2023-03-16 15:00:24 -0700

    Another commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+/**
+*main - this is the main function
+*Return: always 0
+*/
 int main(void)
 {
-/*
-*prints "Programming is like building a multilingual puzzle
-*/
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-
