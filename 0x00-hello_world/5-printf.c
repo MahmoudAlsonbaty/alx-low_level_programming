93fd23d0bb9322fa969c09fa3ba015ba436884f2
MahmoudAlsonbaty <mahmoudAlsonbaty1@gmail.com>
2023-03-16 14:46:16 -0700

    Another commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+/*
+*prints with proper grammar, but the outcome is a piece of art,
+*/
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
